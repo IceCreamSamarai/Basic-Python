68bb3224e53641c1213292b019c1da8398c85a28
IceCreamSamarai <seanr4499@gmail.com>
2023-06-30 16:28:12 +0100

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,35 @@
+# This assigns values to the following variables.
+x = 5
+y = 10
+z = 20
+
+""" This prints the following if y is 
+greater than x. """
+if x < y:
+    print("y is greater than x.")
+
+# This sets all 3 variables to the same value of 50.
+x = y = z = 50
+
+print(x)
+print(y)
+print(z)
+
+""" This sets all 3 variables to
+ there corisponding value. """
+x, y, z = 25, 50, 75
+
+print(x)
+print(y)
+print(z)
+
+# This sets a type of value to each of the following variables values.
+x = int(25)
+y = float(50)
+z = str(75)
+
+""" This prints the exact type of each of the variables values. """
+print(type(x))
+print(type(y))
+print(type(z))
+
